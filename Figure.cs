b2347535ccb0ac3516289768444dd4f29464e0c7
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-16 13:12:30 +0300

    Add files via upload

---
+++ Figure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание2
+{
+
+    class Figure
+    {
+        private string Name;
+        private double Per;
+        public string GetName
+        {
+            get
+            {
+                return Name;
+            }
+        }
+        public double GetPer
+        {
+            get
+            {
+                return Per;
+            }
+        }
+        public Figure(Point a, Point b, Point c)
+        {
+            Name = "треугольник";
+        }
+        public Figure(Point a, Point b, Point c, Point d)
+        {
+            Name = "четырехугольник";
+        }
+        public Figure(Point a, Point b, Point c, Point d, Point e)
+        {
+            Name = "пятиугольник";
+        }
+
+        public double LengthSide(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(b.GetX - a.GetX, 2) + Math.Pow(b.GetY - a.GetY, 2));
+        }
+        public void PerimeterCalculator(Point a, Point b, Point c)
+        {
+            Per = LengthSide(a, b) + LengthSide(b, c) + LengthSide(c, a);
+        }
+        public void PerimeterCalculator(Point a, Point b, Point c, Point d)
+        {
+            Per = LengthSide(a, b) + LengthSide(b, c) + LengthSide(c, d) + LengthSide(d, a);
+        }
+        public void PerimeterCalculator(Point a, Point b, Point c, Point d, Point e)
+        {
+            Per = LengthSide(a, b) + LengthSide(b, c) + LengthSide(c, d) + LengthSide(d, e) + LengthSide(e, a);
+        }
+    }
+}
---
+++ Invoice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание5
+{
+    class Invoice
+    {
+        private int account;
+        private string customer;
+        private string provider;
+        private string article;
+        private int quantity;
+
+        public Invoice(int acc, string cust, string prov)
+        {
+            account = acc;
+            customer = cust;
+            provider = prov;
+        }
+
+        public string Arcicle
+        {
+            set
+            {
+                article = value;
+            }
+            get
+            {
+                return article;
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+        
+        
+
+        public void WithoutNDS()
+        {
+            double sum = account * quantity;
+            Console.WriteLine("Заказчик: " + customer + ". Продавец: " + provider + ". Наименование товара: " + article + ". Количество: " + quantity + ". Цена за 1 шт: " + account + ". Всего без учета НДС: " + sum);
+        }
+        public void WithNDS()
+        {
+            double sum = account * quantity + ((account * quantity) * 0.2);
+            Console.WriteLine("Заказчик: " + customer + ". Продавец: " + provider + ". Наименование товара: " + article + ". Количество: " + quantity + ". Цена за 1 шт: " + account + ". Всего с учетом НДС: " + sum);
+        }
+    }
+}
