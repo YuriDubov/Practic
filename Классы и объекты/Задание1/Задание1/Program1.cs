dbf8a7f3d3348abe3b40fc94a755653dc72a1bcb
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-16 13:39:09 +0300

    Add files via upload

---
+++ Классы и объекты/Задание1/Задание1/Program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Задание1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите значение 1 стороны прямоугольника: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите значение 2 стороны прямоугольника");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Ractangle rec = new Ractangle(a, b);
+
+                Console.WriteLine("Площадь прямоугольника: " + rec.Area);
+                Console.WriteLine("Периметр прямоугольника: " + rec.Perimeter);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
