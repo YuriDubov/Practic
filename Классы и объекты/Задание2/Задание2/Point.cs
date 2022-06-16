bedc8f3b6aeef94277834700851b96bc4435eddd
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-16 13:42:21 +0300

    Add files via upload

---
+++ Классы и объекты/Задание2/Задание2/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание2
+{
+    class Point
+    {
+        private int X;
+        private int Y;
+
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int GetX
+        {
+            get
+            {
+                return X;
+            }
+        }
+        public int GetY
+        {
+            get
+            {
+                return Y;
+            }
+        }
+        
+    }
+}
