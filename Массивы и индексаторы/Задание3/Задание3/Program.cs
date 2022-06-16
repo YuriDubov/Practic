057273ba71e3fb982baa3e470259025e23e1bfb8
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-16 15:28:42 +0300

    Add files via upload

---
+++ Массивы и индексаторы/Задание3/Задание3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Задание3
+{
+    class Program
+    {
+        public static void Sort(int[] arr)
+        {
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    for (int j = i + 1; j < arr.Length; j++)
+                    {
+                        int temp;
+                        if (arr[i] > arr[j])
+                        {
+                            temp = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] array = new int[5] { 5, 4, 8, 10, 12 };
+                Console.WriteLine("Текущий массив: " + String.Join(" ", array));
+                Sort(array);
+                Console.Write("Отсортированный массив: " + String.Join(" ", array));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
