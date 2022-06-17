60e5f8e3dac9ae2cd06747e01eb48fdcb6b02fab
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-17 11:48:10 +0300

    Add files via upload

---
+++ Структуры и их разновидности/Задание2/Задание2/MyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание2
+{
+    class MyClass
+    {
+        public string change;
+
+    }
+}
---
+++ Структуры и их разновидности/Задание2/Задание2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+
+            myClass.change = "не изменено";
+            myStruct.change = "не изменено";
+
+            ClassTaker(myClass);
+            StructTaker(myStruct);
+
+            Console.WriteLine("Класс: " + myClass.change);
+            Console.WriteLine("Структура: " + myStruct.change);
+        }
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+    }
+
+    struct MyStruct
+    {
+        public string change;
+
+    }
+}
