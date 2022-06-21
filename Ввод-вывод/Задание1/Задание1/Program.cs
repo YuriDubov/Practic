ee970502620c3c87db40ac621971f16e6ef26485
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-21 11:53:30 +0300

    Add files via upload

---
+++ Ввод-вывод/Задание1/Задание1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Directory.CreateDirectory($@"C:\\Folder_{i}");
+            }
+            Console.WriteLine("Директории созданы");
+            Console.WriteLine("Удалить директории?\n1. Да\n2. Нет");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 1)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Directory.Delete($@"C:\\Folder_{i}");
+                }
+                Console.WriteLine("Директории удалены");
+            }
+            else if (n == 2)
+                Console.WriteLine("Директории оставлены. Проверьте диск :)");
+            else
+                Console.WriteLine("Такой операции нет");
+            
+            
+        }
+    }
+}
