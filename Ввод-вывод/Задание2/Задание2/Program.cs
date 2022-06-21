3d0ec91e1ea98ec3cad569146d1c3aa0f4fb3af8
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-21 11:54:00 +0300

    Add files via upload

---
+++ Ввод-вывод/Задание2/Задание2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(@"C:\Dir\FileExample.txt", "ahdfjghsjhsn\n731974t461igyvfa");
+            
+            StreamReader sr = new StreamReader(@"C:\Dir\FileExample.txt");
+            Console.WriteLine("Содержимое файла:");
+            for(int i = 0; i < File.ReadAllLines(@"C:\Dir\FileExample.txt").Length; i++)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            
+        }
+    }
+}
