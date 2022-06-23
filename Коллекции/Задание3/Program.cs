19b98e34f7af2d01f047ce609ed99b8b9efa48c1
YuriDubov <107617932+YuriDubov@users.noreply.github.com>
2022-06-23 11:07:03 +0300

    Add files via upload

---
+++ Коллекции/Задание3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Задание3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { };
+            Random r = new Random();
+            int c = r.Next(2, 6);
+
+            Console.WriteLine("Введите " + c + " чисел");
+            for (int i = 0; i < c; i++)
+            {
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            int sum = 0;
+            for (int i = 0; i < c; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("Введите команду:");
+            string command;
+
+            while(true)
+            {
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "sum":
+                        Console.WriteLine("Сумма всех чисел: " + sum);
+                        break;
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Такой команды нет");
+                        break;
+                }
+            }
+            
+            //Console.ReadKey();
+        }
+    }
+}
